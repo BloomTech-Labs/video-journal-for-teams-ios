b74ff548b2c2dfe949c02889028e8b6602d7d4ce
scottharris86 <scottharrisdev@gmail.com>
2020-05-14 12:24:56 -0400

    inital project creation

---
+++ TeemReel/TeemReel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeemReel
+//
+//  Created by scott harris on 5/14/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
