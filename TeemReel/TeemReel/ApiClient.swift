53e9c9c45793434ecd4ca852635f40b3b89811e6
scottharris86 <scottharrisdev@gmail.com>
2020-05-18 15:53:01 -0400

    organizations and teams tables

---
+++ TeemReel/TeemReel/ApiClient.swift
@@ -0,0 +1,93 @@
+//
+//  ApiClient.swift
+//  TeemReel
+//
+//  Created by scott harris on 5/15/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import Foundation
+
+
+class ApiClient {
+    let baseURL = URL(string: "https://video-journal.herokuapp.com/api/")!
+    
+    func fetchOrganizations(userId: Int, completion: @escaping ([Organization]?, Error?) -> Void) {
+        let urlPath = baseURL.appendingPathComponent("users/\(userId)/organizations")
+        
+        var urlRequest = URLRequest(url: urlPath)
+        urlRequest.addValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjE5NywiaWF0IjoxNTg5ODI3MjYzLCJleHAiOjE1ODk4NzA0NjN9.0zPkZM66ISPSkxt-Q5aNzTAENHxw11Ua1AnW9HNALIU", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                print("Network Error: \(error)")
+                completion(nil, error)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                let error = NSError(domain: "com.teamreel", code: 101, userInfo: nil)
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                let error = NSError(domain: "com.teamreel", code: 105, userInfo: nil)
+                completion(nil, error)
+                return
+            }
+            
+            do {
+                let orgs = try JSONDecoder().decode([Organization].self, from: data)
+                completion(orgs, nil)
+                return
+            } catch {
+                completion(nil, error)
+                return
+            }
+            
+        }.resume()
+        
+    }
+    
+    func fetchTeams(for organizationId: Int, completion: @escaping ([Team]?, Error?) -> Void) {
+        let urlPath = baseURL.appendingPathComponent("organizations/\(organizationId)/teams")
+        
+        var urlRequest = URLRequest(url: urlPath)
+        urlRequest.addValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjE5NywiaWF0IjoxNTg5ODI3MjYzLCJleHAiOjE1ODk4NzA0NjN9.0zPkZM66ISPSkxt-Q5aNzTAENHxw11Ua1AnW9HNALIU", forHTTPHeaderField: "Authorization")
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                print("Network Error: \(error)")
+                completion(nil, error)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                let error = NSError(domain: "com.teamreel", code: 101, userInfo: nil)
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                let error = NSError(domain: "com.teamreel", code: 105, userInfo: nil)
+                completion(nil, error)
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                let teams = try decoder.decode([Team].self, from: data)
+                completion(teams, nil)
+                return
+            } catch {
+                completion(nil, error)
+                return
+            }
+            
+        }.resume()
+        
+    }
+    
+}
